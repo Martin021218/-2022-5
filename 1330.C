d2d70dea26c8094c87f80a623046e35a5415b1ba
Martin021218 <105496179+Martin021218@users.noreply.github.com>
2022-05-14 15:25:04 +0800

    新手正式上路

---
+++ 1330.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	printf("nmsl\n");
+	printf("%d\n", 100);
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+
+	return 0;
+}
+
