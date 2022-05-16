569e3319982d999850db63bc72fe276dbba3395b
Martin021218 <105496179+Martin021218@users.noreply.github.com>
2022-05-16 18:29:27 +0800

    Create 求勾股定理最长边c_plus_1.cpp

---
+++ 求勾股定理最长边c_plus_1.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include <iostream>
+
+double a1()
+{
+	std::cout << "请输入a边边长：";
+	double a11{};
+	std::cin >> a11;
+	return a11;
+}
+
+double b1()
+{
+	std::cout << "请输入b边边长：";
+	double b11{};
+	std::cin >> b11;
+	return b11;
+}
+
+int main()      //求勾股定理最长边c
+{
+	double a{a1()};
+	double b{b1()};
+
+	std::cout << "a=" << a << "     b=" << b << "\n";					 //eg.a=?      b=?
+	std::cout << "c∧2=" << a << "∧2+" << b << "∧2\n";				     //   c²＝a²＋b²   
+	std::cout << "c=√(" << pow(a, 2) << "+" << pow(b, 2) << ")\n";       //    c²=√(a²＋b²)  
+	std::cout << "c=" << sqrt(pow(a, 2) + pow(b, 2));                    //    c=?
+
+	return 0;
+}
