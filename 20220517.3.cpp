6ba269be1adcf49796b2872077794640e53963b3
Martin021218 <105496179+Martin021218@users.noreply.github.com>
2022-05-17 11:14:26 +0800

    求勾股定理最长边1.0.cpp

---
+++ 20220517.3.cpp
@@ -0,0 +1,31 @@
+﻿//求勾股定理最长边
+#include "pch.h"
+#include <iostream>
+
+double h()
+{
+	double a{};
+		std::cout << "请输入a边边长：";
+	std::cin >> a;
+	return pow(a, 2);				//返回a边平方的值
+}
+
+double i()
+{
+	double b{};
+	std::cout << "请输入b边边长：";
+	std::cin >> b;
+	return pow(b, 2);				//返回b边平方的值
+}
+
+void dm(double sq)
+{
+	std::cout << "c=" << sqrt(sq);
+}
+
+int main()
+{
+	double z{ h() + i() };
+	dm(z);
+	return 0;
+}
